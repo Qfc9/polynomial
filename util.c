3cd08996731c8fde590a0dfb1f3ff97a02ed5ad1
Elijah Harmon <elijahharmon@gmail.com>
2018-01-13 11:05:15 -0500

    Added basic documentation to util

--- util.c
+++ util.c
@@ -1,25 +1,33 @@
+/*  util.c  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "util.h"
 
-
+// Returns the Char values of an int
 int intToChar(int a)
 {
     return a + 48;
 }
 
+// Takes a number and returns the unicode subscript version of that number
 char *subscript(unsigned int normalExp)
 {
+    // Making the Subscript String 
     char *subExp = malloc(sizeof(*subExp) * 64);
     strncpy(subExp, "\0", 64);
     strncpy(subExp, "x", 1);
+
+    // Making the expression into a string
     char strExp[64];
     sprintf(strExp, "%63u", normalExp);
 
+    // Looping through every number in the values passed
     for(unsigned int i = 0; i < strlen(strExp); i++)
     {
+        // Setting the appopriate subscript
         switch(strExp[i] - 48)
         {
             case 2:
--- util.h
+++ util.h
@@ -1,9 +1,9 @@
+/*  util.h  */
 
 #ifndef UTIL_H
- #define UTIL_H
+#define UTIL_H
 
 int intToChar(int a);
-
 char *subscript(unsigned int normalExp);
 
 #endif
